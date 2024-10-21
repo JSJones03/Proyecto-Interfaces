5e3bd31b3909966d7c0610688209bb34a5673642
alumno <alumno@dam205>
2024-10-21 10:44:14 +0200

    Primer commit netbeans

---
+++ src/main/java/com/mycompany/ejercicioeval/EjercicioEval.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicioeval;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioEval {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
